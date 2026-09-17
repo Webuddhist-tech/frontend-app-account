42df3c121095c37fab26b66225f21284758dac09
Tenzin Choeying <tenzinchoeying@Tenzins-MacBook-Pro-4.local>
2026-09-17 13:06:14 +0530

    fix(account): JumpNav notifications gating and scroll sync

--- src/account-settings/data/selectors.js
+++ src/account-settings/data/selectors.js
@@ -1,6 +1,9 @@
 import { createSelector, createStructuredSelector } from 'reselect';
+import { selectShowPreferences } from '../../notification-preferences/data/selectors';
 import { siteLanguageListSelector, siteLanguageOptionsSelector } from '../site-language';
 import { compareVerifiedNamesByCreatedDate } from '../../utils';
+
+const showPreferencesSelector = selectShowPreferences();
 
 export const storeName = 'accountSettings';
 
@@ -243,6 +246,7 @@ export const accountSettingsPageSelector = createSelector(
   mostRecentVerifiedNameSelector,
   sortedVerifiedNameHistorySelector,
   countriesSelector,
+  showPreferencesSelector,
   (
     accountSettings,
     siteLanguageOptions,
@@ -261,6 +265,7 @@ export const accountSettingsPageSelector = createSelector(
     mostRecentVerifiedName,
     verifiedNameHistory,
     countriesCodesList,
+    showPreferences,
   ) => ({
     siteLanguageOptions,
     siteLanguage,
@@ -282,6 +287,7 @@ export const accountSettingsPageSelector = createSelector(
     mostRecentVerifiedName,
     verifiedNameHistory,
     countriesCodesList,
+    showPreferences,
   }),
 );
 
